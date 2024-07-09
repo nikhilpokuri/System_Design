b98957fa60cc14fea31dd69e7f31a8724985cb1a
nikhil <nikhilpokuri143@gmail.com>
2024-07-09 16:09:37 +0530

    interface_segregation

--- Low_Level_Design/solid_principles/interface_segregation.py
+++ Low_Level_Design/solid_principles/interface_segregation.py
@@ -51,41 +51,42 @@ print()
 # ---------------------------------------------------------
 
 # Below, we have separated the Authenticator from LoginProcessor to achieve Interface segregation.
+# But we are depending on another class, 
+# this violates Dependency Inversion Principle, we'll achieve Dependency Inversion in dependency_inversion.py
 
 class LoginProcessor(ABC):
     @abstractmethod
     def login(self, mail, password):
         pass
 
-class Authenticator(ABC):
-    @abstractmethod
-    def twoStepAuthentication(self):
-        pass
-
-class Gmail(LoginProcessor, Authenticator):
-    def login(self, mail, password):
-        print("[TWO STEP AUTHENTICATOR]: Please Verify that it's you.!!")
-        return self.twoStepAuthentication()
-    
+class Authenticator:
     def twoStepAuthentication(self):
         print("[TWO STEP AUTHENTICATOR]: Authentication Successfull")
+
+class Gmail(LoginProcessor):
+    def __init__(self, authenticator: Authenticator) -> None:
+        self.authenticator = authenticator
+
+    def login(self, mail, password):
+        print("[TWO STEP AUTHENTICATOR]: Please Verify that it's you.!!")
+        self.authenticator.twoStepAuthentication()
         return "Gmail Logged In\n"
 
-class Facebook(LoginProcessor, Authenticator):
+class Facebook(LoginProcessor):
+    def __init__(self, authenticator: Authenticator) -> None:
+        self.authenticator = authenticator
+
     def login(self, mail, password):
         print("[TWO STEP AUTHENTICATOR]: Please Verify that it's you.!!")
-        return self.twoStepAuthentication()
-    
-    def twoStepAuthentication(self):
-        print("[TWO STEP AUTHENTICATOR]: Authentication Successfull")
+        self.authenticator.twoStepAuthentication()
         return "Facebook Logged In\n"
 
 class Zoom(LoginProcessor):
     def login(self, mail, password):
         return "Zoom Logged In\n"
 
-mail = Gmail()
-facebook = Facebook()
+mail = Gmail(Authenticator())
+facebook = Facebook(Authenticator())
 zoom = Zoom()
 print(mail.login("nick@gmail.com", "password"))
 print(facebook.login("nick@gmail.com", "password"))
